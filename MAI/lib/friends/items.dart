c63dbf2bcacf5ec5b0de930371cf651d94feae5e
zassous <wwwmasami1227@gmail.com>
2019-06-28 19:03:39 +0900

    Update items.dart

--- MAI/lib/friends/items.dart
+++ MAI/lib/friends/items.dart
@@ -4,8 +4,14 @@ import 'package:flutter/material.dart';
 class _Friend {
   String name, status;
   File iconImage;
+  bool isTomodaTimpo;
 
-  _Friend({this.name, this.status, this.iconImage});
+  _Friend({
+    this.name,
+    this.status,
+    this.iconImage,
+    this.isTomodaTimpo: false,
+  });
 
   Widget _widget(){
     return Container(
@@ -14,11 +20,11 @@ class _Friend {
       height: 100,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.all(Radius.circular(10)),
-        color: Colors.grey,
+        color: isTomodaTimpo ? Colors.yellow : Colors.grey,
       ),
       child: Row(
         children: <Widget>[
-          //iconImage, TODO: あとでやるで（あいこんやで
+          //iconImage, TODO: あとでやるで（あいこんやで）
           Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -57,8 +63,9 @@ class _FriendsState extends State<Friends> {
     ),
     _Friend(
       name: 'たかし',
-      status: 'あじうgybhににjんjにうぬn',
+      status: 'あじ',
       iconImage: null,
+      isTomodaTimpo: true,
     ),
     _Friend(
       name: 'なめこ',
@@ -75,12 +82,36 @@ class _FriendsState extends State<Friends> {
       status: 'あーめんどくせー',
       iconImage: null,
     ),
+        _Friend(
+      name: 'ジーメン',
+      status: 'じーめんどくせー',
+      iconImage: null,
+    ),
+        _Friend(
+      name: 'ダーメン',
+      status: 'だーめんどくせー',
+      iconImage: null,
+      isTomodaTimpo: true,
+    ),
   ];
 
+  List<_Friend> _sortedFriendList = [];
+
   Widget _body() {
+    _friendList.forEach((_f) {
+      if(_f.isTomodaTimpo){
+        _sortedFriendList.add(_f);
+      }
+    });
+    _friendList.forEach((_f) {
+      if(!_f.isTomodaTimpo){
+        _sortedFriendList.add(_f);
+      }
+    });
+
     return ListView.builder(
       itemBuilder: ((BuildContext context, int index) {
-        return _friendList[index]._widget();
+        return _sortedFriendList[index]._widget();
       }),
       itemCount: _friendList.length,
     );
