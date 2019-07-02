7fcc19f6d0b526da9aa63e4b6d9ff36d6acef928
50m-regent <32361219+50m-regent@users.noreply.github.com>
2019-07-02 19:17:24 +0900

    tabtabtabtab

--- MAI/lib/diary/items.dart
+++ MAI/lib/diary/items.dart
@@ -3,29 +3,29 @@ import 'package:mai/constants.dart';
 import 'new_diary/main.dart';
 
 Widget newDiaryIcon(BuildContext context) {
-    final Size _displaySize = MediaQuery.of(context).size;
-    final double _iconSize = _displaySize.width / 12;
-    return FloatingActionButton.extended(
-      onPressed: () {
-        showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return NewDiaryPage();
-          }
-        );
-      },
-      label: Text(
-        '新しい日記を作成',
-        style: TextStyle(
-          fontSize: 20,
-        ),
+  final Size _displaySize = MediaQuery.of(context).size;
+  final double _iconSize = _displaySize.width / 12;
+  return FloatingActionButton.extended(
+    onPressed: () {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return NewDiaryPage();
+        }
+      );
+    },
+    label: Text(
+      '新しい日記を作成',
+      style: TextStyle(
+        fontSize: 20,
       ),
-      icon: Icon(
-        Icons.add,
-        color: Colors.white,
-        size: _iconSize,
-      ),
-      tooltip: "新しい日記",
-      backgroundColor: ICON_COLOR,
-    );
-  }
+    ),
+    icon: Icon(
+      Icons.add,
+      color: Colors.white,
+      size: _iconSize,
+    ),
+    tooltip: "新しい日記",
+    backgroundColor: ICON_COLOR,
+  );
+}
