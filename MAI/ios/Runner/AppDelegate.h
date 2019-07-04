1462b32b806e570f389c387be2aba3a908e85987
50m-regent <32361219+50m-regent@users.noreply.github.com>
2019-07-04 22:44:24 +0900

    Revert "firebase はじめますたwww"
    This reverts commit d20ad4459aa87956aaf9256506ba09d551d4589f.

--- MAI/ios/Runner/AppDelegate.h
+++ MAI/ios/Runner/AppDelegate.h
@@ -4,14 +4,3 @@
 @interface AppDelegate : FlutterAppDelegate
 
 @end
-
-@import UIKit;
-@import Firebase;
-
-@implementation AppDelegate
-
-- (BOOL)application:(UIApplication *)application
-didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    [FIRApp configure];
-    return YES;
-}
