f25f30259e4f0e6258f0af4b876bddb447f68ea0
issan719 <snym719a@gmail.com>
2019-07-21 14:49:15 +0900

    pushしたらメントスみたいに爆発するんかな

--- MAI/lib/diary/opened_diary/main.dart
+++ MAI/lib/diary/opened_diary/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import '../diary.dart';
+import 'package:intl/intl.dart';
+import 'package:mai/constants.dart';
 
 class OpenedDiary extends StatefulWidget {
   final Diary diary;
@@ -12,14 +14,60 @@ class OpenedDiary extends StatefulWidget {
 
 class _OpenedDiaryState extends State<OpenedDiary> {
   final Diary diary;
+  final _diaryheight = displaySize.height / 2.5;
 
   _OpenedDiaryState(this.diary);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: MyColors.background,
+      appBar: AppBar(
+        backgroundColor: MyColors.box,
+        title: Text(
+          diary.name,
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
       body: Container(
-        child: Text('tt'),
+        margin: EdgeInsets.all(margin),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Align(
+              alignment: Alignment.topLeft,
+              child: Text(
+                DateFormat('M月d日').format(
+                    DateTime.parse(diary.date.toString())
+                ).toString(),
+                style: MyTextStyle().bigBold,
+              ),
+            ),
+            Container(
+              child:Column(
+                children: <Widget>[
+                  //Image.file(diary.image),
+                  Container(
+                    height: _diaryheight,
+                    child: ListView.builder(
+                      itemCount: 1,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Text(
+                          "${diary.text}",
+                          style: MyTextStyle().normal,
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
